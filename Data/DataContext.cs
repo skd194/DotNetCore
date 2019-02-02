e9debbc6fe5e30f7a2b5edb031cb1411e342a348
Shyam Kumar S <shyam@distinctinfotech.com>
2019-02-02 07:11:44 +0530

    initial commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+using Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Value> Values { get; set; }
+    }
+}
